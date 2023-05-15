2e519a8544ffe9ee198f481466420b89586a9a1a
Shubham Singh <shubham37@outlook.com>
2023-05-15 21:17:20 +0530

    pickling in python refactored

---
+++ advance_topics/pickling.py
@@ -0,0 +1,25 @@
+import pickle
+
+
+def serialize():
+    """
+    Serializing python objects
+    """
+    f = open( 'pickled.txt', 'wb' )
+    dct = {"name": "Shubham", "age": 23, "Gender": "Male"}
+    pickle.dump( dct, f )
+    f.close()
+
+
+def deserialize():
+    """
+    Serializing python objects
+    :return obj:
+    """
+    f = open( 'pickled.txt', 'rb' )
+    dct_obj = pickle.load( f )
+    print( dct_obj )
+    f.close()
+
+serialize()
+deserialize()
